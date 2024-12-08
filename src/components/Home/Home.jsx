84fed6ac676e99f6595972c8fa06dc87e68cb907
kodegopj <pauljohncunanan5@gmail.com>
2024-12-09 00:08:48 +0800

    changing to captaion

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ import Footer from '../Footer/Footer.jsx';
 import Contact from '../Contact/Contact.jsx';
 import Skills from "../Reels/Skills";
 import Introduction from "../Introduction/About.jsx";
-import Featured from '../Features.jsx/Featured.jsx';
+import Featured from '../Features/Featured.jsx';
 import Areyou from '../What/Areyou.jsx';
 import Ourvideo from '../Ourvideoeditingservices/Ourvideo.jsx';
 import Whatwedo from '../What we do/Whatwedo.jsx';
